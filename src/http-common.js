966da74f18e5581069d64b61e13f29f6284a5c56
Leandro Paz <leandropaz@gmail.com>
2020-09-06 15:01:16 -0300

    (fix): add / to baseURL.

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://leandropaz-api-grades.herokuapp.com',
+  baseURL: 'https://leandropaz-api-grades.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
